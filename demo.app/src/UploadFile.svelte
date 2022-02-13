dfdcf931b978c9dccdd18a8596558c905af18a2c
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-02-14 03:20:34 +0400

    add a demo app

---
+++ demo.app/src/UploadFile.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import upchunk from "../../lib/cloudinaryUpChunk";
+
+  const onchangeInput = async (e) => {
+    const file = e.target.files[0];
+    await upchunk(file);
+  };
+</script>
+
+<input type="file" id="file-upload" on:change={onchangeInput} />
